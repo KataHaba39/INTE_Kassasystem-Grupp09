40e49ad997caba954d494e6eebd7ed5847d87f02
KataHaba39 <anton.stodberg@gmail.com>
2025-10-13 22:46:42 +0200

    provat egen branch

--- src/main/java/com/grupp09/kassasystem/App.java
+++ src/main/java/com/grupp09/kassasystem/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         var sum = Calculator.Add(40, 2);
         System.out.println("Grupp 09090909 Answer: " + sum);
+        // egen branch
     }
 
 }
