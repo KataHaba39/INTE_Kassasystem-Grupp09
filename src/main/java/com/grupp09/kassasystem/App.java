1b2e0b728bcc10afd1247b4b69fb62b7aa6f79f0
KataHaba39 <anton.stodberg@gmail.com>
2025-10-13 22:17:07 +0200

    Startar upp projektet med Java 21 + JUnit 5

---
+++ src/main/java/com/grupp09/kassasystem/App.java
@@ -0,0 +1,10 @@
+package com.grupp09.kassasystem;
+
+public class App {
+
+    public static void main(String[] args) {
+        var sum = Calculator.Add(40, 2);
+        System.out.println("CloudAcademy DevOps 2020!! Answer: " + sum);
+    }
+
+}
