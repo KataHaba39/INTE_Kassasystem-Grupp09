b3185cbbaaa9967a0107c1588e07e4b133f8f5f3
Oliver Eriksson <oliver.j.h.eriksson@gmail.com>
2025-10-14 14:05:54 +0200

    Lade till MoneyTest klass

---
+++ src/test/java/com/grupp09/kassasystem/MoneyTest.java
@@ -0,0 +1,12 @@
+package com.grupp09.kassasystem;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    @Test
+    void money_class_sets_attributes(){
+        Money money = new Money();
+        assertEquals(0.0d, money.getValue());
+    }
+}
