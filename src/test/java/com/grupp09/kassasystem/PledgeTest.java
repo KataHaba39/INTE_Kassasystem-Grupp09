bde6bb0fd904298ed11390b5fbd14defff7228df
Oliver Eriksson <oliver.j.h.eriksson@gmail.com>
2025-10-18 13:22:38 +0200

    Implementerade PledgeTest

---
+++ src/test/java/com/grupp09/kassasystem/PledgeTest.java
@@ -0,0 +1,28 @@
+package com.grupp09.kassasystem;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+public class PledgeTest {
+
+    @Test
+    void pledge_returnsCorrectPledge(){
+        FixedPriceItem softDrinkItem = new FixedPriceItem("Soda", Money.toMoney(10.0d), ItemGroups.DRYCK);
+        FixedPriceItem alcoholicBeverage = new FixedPriceItem("Beer", Money.toMoney(20.0d), ItemGroups.DRYCK_ALKOHOL);
+
+        assertEquals(Money.toMoney(1.0d), Pledge.pledgeOf(softDrinkItem));
+        assertEquals(Money.toMoney(2.0d), Pledge.pledgeOf(alcoholicBeverage));
+    }
+
+    @Test
+    void pledge_addsCorrectPledge(){
+        FixedPriceItem softDrinkItem = new FixedPriceItem("Soda", Money.toMoney(10.0d), ItemGroups.DRYCK);
+        FixedPriceItem alcoholicBeverage = new FixedPriceItem("Beer", Money.toMoney(20.0d), ItemGroups.DRYCK_ALKOHOL);
+
+        assertEquals(Money.toMoney(11.0d), Pledge.priceWithPledge(softDrinkItem));
+        assertEquals(Money.toMoney(22.0d), Pledge.priceWithPledge(alcoholicBeverage));
+    }
+    
+}
