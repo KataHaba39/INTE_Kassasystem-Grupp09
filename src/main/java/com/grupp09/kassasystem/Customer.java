e6b0c2898917f772e7b9f5babf6a366809f75c53
AssilAlda <meetandtreat.team@gmail.com>
2025-10-25 16:45:03 +0200

    Lagt till membership i customer samt lagt till tester för det

--- src/main/java/com/grupp09/kassasystem/Customer.java
+++ src/main/java/com/grupp09/kassasystem/Customer.java
@@ -5,16 +5,26 @@ public class Customer {
     private String name;
     private String phoneNumber;
     private String email;
+    private Membership membership;
 
     public Customer (String customerId, String name, String phoneNumber, String email) {
         this.customerId = customerId;
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
+        this.membership = null;
     }
 
     public Customer(String customerId, String name) {
         this(customerId, name, null, null);
+    }
+
+    public Customer(String customerId, String name, String phoneNumber, String email, Membership membership) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.membership = membership;
     }
 
     public String getCustomerId() {
@@ -33,12 +43,25 @@ public class Customer {
         return email;
     }
 
+    public Membership getMembership() {
+        return membership;
+    }
+
+    public void setMembership(Membership membership) {
+        this.membership = membership;
+    }
+
+    public boolean hasMembership(Membership membership) {
+        return membership != null;
+    }
+
     @Override
     public String toString() {
         return "Customer{" + "Id='" + customerId + '\'' + 
         ", Name='" + name + '\'' +
         ", Phone='" + phoneNumber + '\'' +
         ", email='" + email + '\'' +
+        ", membership=" + (membership != null ? "Yes" : "No") +
         '}';
     }
 }
